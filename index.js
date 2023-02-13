6b6bf2d0aabdb17f7bd13cfc57894902171b039e
SSam0419 <leemingchun419@gmail.com>
2023-02-13 13:07:51 +0800

    add type effect

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
+
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
     var elementVisible = 150;
+
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
@@ -11,7 +13,5 @@ function reveal() {
     }
   }
 }
-window.addEventListener("scroll", reveal);
 
-// To check the scroll position on page load
-reveal();
+window.addEventListener("scroll", reveal);
